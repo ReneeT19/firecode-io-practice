c587e7b53ae1432795b726eaadd0441a42653aa5
Renee Thomsen <rcthomsen19@gmail.com>
2021-05-11 21:57:00 -0400

    Added charAt() method to reverse a string

--- reverseString/src/com/company/reverseString.java
+++ reverseString/src/com/company/reverseString.java
@@ -7,12 +7,12 @@ import java.util.List;
 public class reverseString {
 
     public static void main(String[] args) {
-	// write your code here
+        // write your code here
         System.out.println(reverseString("abc"));
     }
 
-    public static String reverseString(String str){
-    //using basic core Java
+    public static String reverseString(String str) {
+        //using basic core Java - char array
 //        if(str == null || str.equals("")) return str;
 //        char[] in = str.toCharArray();
 //        int begin=0;
@@ -28,16 +28,32 @@ public class reverseString {
 //
 //        return new String(in);
 
-    //using ArrayList and .reverse()
-        List<Character> list = new ArrayList<Character>();
-        for(char c: str.toCharArray()) {
-            list.add(c);
+        //using basic core Java - charAt()
+        // String inputString = str;
+        String outputString = "";
+
+        if (str == null || str.equals("")) {
+            outputString = str;
         }
-        Collections.reverse(list);
-        StringBuilder builder = new StringBuilder(list.size());
-        for(Character c: list) {
-            builder.append(c);
+        //length() yields a NullPointerException as you can't call methods on null variables therefore else is needed here.
+        else {
+            for (int i = str.length() - 1; i >= 0; i--) {
+                outputString = outputString + str.charAt(i);
+            }
         }
-        return builder.toString();
+        return outputString;
     }
 }
+    //using ArrayList and .reverse()
+//        List<Character> list = new ArrayList<Character>();
+//        for(char c: str.toCharArray()) {
+//            list.add(c);
+//        }
+//        Collections.reverse(list);
+//        StringBuilder builder = new StringBuilder(list.size());
+//        for(Character c: list) {
+//            builder.append(c);
+//        }
+//        return builder.toString();
+
+
