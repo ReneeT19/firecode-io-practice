962634aeb27b0c645b10298693937111f1eb40ec
Renee Thomsen <rcthomsen19@gmail.com>
2021-05-12 22:06:32 -0400

    insert node at the tail of a linked list

---
+++ insertAtTail/src/com/company/insertAtTail.java
@@ -0,0 +1,24 @@
+package com.company;
+
+public class insertAtTail {
+
+    public ListNode insertAtTail(ListNode head, int data) {
+
+        ListNode newNode = new ListNode(data);
+
+        if(head == null) {
+            head = newNode;
+        } else {
+            ListNode last = head;
+            while (last.next != null) {
+                last = last.next;
+            }
+            last.next = newNode;
+        }
+        return head;
+    }
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
