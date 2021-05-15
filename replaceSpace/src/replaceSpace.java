f1a1e989adad8ac520001858c5927f2d487fcf34
Renee Thomsen <rcthomsen19@gmail.com>
2021-05-14 21:44:44 -0400

    added replaceAll approach

--- replaceSpace/src/replaceSpace.java
+++ replaceSpace/src/replaceSpace.java
@@ -1,6 +1,7 @@
 public class replaceSpace {
     public static void main(String[] args){
         System.out.println(replace("This is a test", "*"));
+        System.out.println(replace2("This is another test", "/"));
     }
 
     public static String replace(String a, String b) {
@@ -10,5 +11,11 @@ public class replaceSpace {
                 return a.replace(" ", b);
             }
         } return a;
+    }
+
+    //use replaceAll
+    public static String replace2(String a, String b) {
+        a = a.replaceAll(" ", b);
+        return a;
     }
 }
